bb5c24249df250e4f87bb0f37ace45f20fe3c690
lazy_owl <slimsly11@gmail.com>
2023-02-16 18:11:54 +0600

    add key to product images fix

--- resources/scripts/product/update_image.sql
+++ resources/scripts/product/update_image.sql
@@ -1,2 +1,3 @@
 insert into product_images (product_id, path, position, key)
-values (:product_id, :path, :position, :key);
+values (:product_id, :path, :position, :key)
+on conflict (key) do update set path = :path;
