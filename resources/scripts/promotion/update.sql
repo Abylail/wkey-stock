8b7a0dd1f2c1e0bba712d2fcf3d847e49e09339b
Абылай <abylay@Air-Abylaj.Dlink>
2023-03-14 01:11:49 +0600

    fix update promotion

--- resources/scripts/promotion/update.sql
+++ resources/scripts/promotion/update.sql
@@ -1,2 +1,6 @@
-UPDATE promotions SET title_ru = :title_ru,
-                  where code = :code;
+UPDATE promotions
+SET title_ru = :title_ru,
+    title_kz = title_kz,
+    description_ru = description_ru,
+    description_kz = description_kz
+where code = :code;
